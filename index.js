1bffbe72487e80792623dec280fc384125d58a2a
uylongLaptop <usong@paragoniu.edu.kh>
2024-06-08 15:00:20 +0700

    fully function backspace , not with extra with space , working timer , display , new backgroundcolor

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const words = 'Innovation drives Innovation drives progress fueling advancements
 // console.log(words);
 const wordsCount = words.length;
 
+const gameTime = 30 ; //30 sec
+window.timer = null;
+window.gameStart = null;
+
 function addClass(element, nameClass) {
-    element.className += ' ' + nameClass;
+    if (element !== null) {
+        element.className += ' ' + nameClass;
+    }
 }
 function removeClass(element, nameClass) {
-    element.className = element.className.replace(nameClass, '')
+    if (element !== null) {
+        element.className = element.className.replace(nameClass, '')
+    }
 }
 //return random words
 function randomWord() {
@@ -29,6 +37,8 @@ function newGame() {
     // document.querySelector(".word").classList.add("current");
     addClass(document.getElementsByClassName("word")[0], 'current');
     addClass(document.getElementsByClassName("letter")[0], 'current');
+    window.timer = null;
+
 }
 document.getElementById("game").addEventListener('keyup', (e) => {
     const key = e.key;
@@ -37,11 +47,22 @@ document.getElementById("game").addEventListener('keyup', (e) => {
     const expectedkey = currentLetter?.innerHTML || ' '; //So, the main difference is that the optional chaining operator (?.) prevents an error if currentLetter is null or undefined, while the direct property access (currentLetter.innerHTML) will throw an error in that case.  if one false it's goes to the other
     console.log({ key, expectedkey });
     const isLetter = key.length === 1 && key != ' ';
-
     //check space typed
     const isSpace = key === ' ';
     const isBackspace = key === 'Backspace';
     const isFirstLetter = currentLetter === currentWord.firstChild;
+
+    if (!window.timer && isLetter) { //not null //only in true
+        window.timer = setInterval(() => {
+            if (!window.gameStart) { //
+                window.gameStart = (new Date()).getSeconds(); //25 //get current time using data object //static
+            }
+            const currentTime = (new Date()).getSeconds(); //26
+            const sLeft = gameTime - (currentTime - window.gameStart); // 30 - (26 -25) // 30 -15 = 29
+            document.getElementById("info").innerHTML = sLeft - 1;
+        }, 1000);
+    }
+
     if (isLetter) { //check this first
         if (currentLetter) {
             addClass(currentLetter, key === expectedkey ? 'correct' : 'incorrect')
@@ -56,14 +77,6 @@ document.getElementById("game").addEventListener('keyup', (e) => {
             currentWord.appendChild(incorrectLetterwhenspace);
         }
     }
-    // if (key.length === 1 && key != ' ') {
-    //     if (key === expectedkey) {
-    //         addClass(document.querySelector(".letter.current"), 'correct');
-    //     }
-    //     else {
-    //         addClass(document.querySelector(".letter.current"), 'incorrect');
-    //     }
-    // }
     if (isSpace) { //check if there a space? we type " "
         if (expectedkey !== ' ') { //check if the expectedkey is ' ' or not (mean space in mid)
             const letterToinvalidate = [...document.querySelectorAll('.word.current .letter:not(.correct)')]; //Have the letter class, Do not have the correct class, And are inside an element with both the word and current classes. //rest of the word in div // only the rest & inncorrect 1
@@ -82,28 +95,48 @@ document.getElementById("game").addEventListener('keyup', (e) => {
 
     }
     if (isBackspace) {//check if there a backspace? we type " "
-        if (currentLetter && isFirstLetter) { //for ah space // do class
-            //make prev word current, last letter current
-            removeClass(currentWord, 'current');
-            addClass(currentWord.previousSibling, 'current');
-            removeClass(currentLetter, 'current');
-            addClass(currentWord.previousSibling.lastChild, 'current');
-            removeClass(currentWord.previousSibling.lastChild, 'correct');
-            removeClass(currentWord.previousSibling.lastChild, 'incorrect');
+        const extraIncorrectLetters = currentWord.querySelectorAll('.letter.incorrect.extra');
+        if (extraIncorrectLetters.length > 0) {
+            const lastExtraLetter = extraIncorrectLetters[extraIncorrectLetters.length - 1];
+            currentWord.removeChild(lastExtraLetter);
+        }
+        else {
+            if (currentLetter && isFirstLetter) {  //for space purspose
+                if (currentWord.previousElementSibling) { // first letter
+                    removeClass(currentWord, 'current');
+                    addClass(currentWord.previousSibling, 'current');
+                    removeClass(currentLetter, 'current');
 
+                    addClass(currentWord.previousSibling.lastChild, 'current');
+                    removeClass(currentWord.previousSibling.lastChild, 'incorrect');
+                    removeClass(currentWord.previousSibling.lastChild, 'correct');
+                }
+            }
+            if (currentLetter && !isFirstLetter) {
+                removeClass(currentLetter, 'current');
+                addClass(currentLetter.previousSibling, 'current');
+                removeClass(currentLetter.previousSibling, 'incorrect');
+                removeClass(currentLetter.previousSibling, 'correct');
+            }
+            if (!currentLetter) {   //space in the middle of words delete space
+                addClass(currentWord.lastChild, 'current');
+                removeClass(currentWord.lastChild, 'incorrect');
+                removeClass(currentWord.lastChild, 'correct');
+            }
         }
-        if (currentLetter && !isFirstLetter) {
-            removeClass(currentLetter, 'current');
-            addClass(currentLetter.previousSibling, 'current');
-            removeClass(currentLetter.previousSibling, 'correct');
-            removeClass(currentLetter.previousSibling, 'incorrect');
-        }
-        if (!currentLetter) {
-            addClass(currentWord.lastChild, 'current');
-            removeClass(currentWord.lastChild, 'correct');
-            removeClass(currentWord.lastChild, 'incorrect');
-        }
+
     }
+
+    if (currentWord.getBoundingClientRect().top > 250) {
+        const wordsline = document.getElementById('words');
+        const margin = parseInt(wordsline.style.marginTop || "0px"); //making it add more vea turn px
+        console.log(margin);
+        wordsline.style.marginTop = (margin - 45) + 'px'; //vea recurisive 
+    }
+    // console.log(currentWord.getBoundingClientRect().top)
+    // from the top
+
+
     //move cursor
     const nextLetterorword = document.querySelector('.letter.current'); //let's try
     const nextWord = document.querySelector('.word.current');
@@ -114,6 +147,7 @@ document.getElementById("game").addEventListener('keyup', (e) => {
     //.....Bottom
     cursor.style.top = (nextLetterorword || nextWord).getBoundingClientRect().bottom + 1 + 'px';
     cursor.style.left = (nextLetterorword || nextWord).getBoundingClientRect()[nextLetterorword ? 'left' : 'right'] + 'px';
+
 })
 
 
